766d3be5a10b58433478800278bffcca4d43a1e2
SRINU <vasu_anand01@yahoo.co.in>
2024-02-02 10:05:56 +0530

    2

--- main.tf
+++ main.tf
@@ -125,6 +125,7 @@ resource "null_resource" "ansible"{
 
     inline = [
       "sudo labauto ansible",
+      "sudo set-hostname -skip-apply ${var.component}",
       "ansible-pull -i localhost, -U https://github.com/SRINU42/roboshop-ansible2 main.yml -e env=${var.env} -e role_name=${var.component}",
     ]
   }
